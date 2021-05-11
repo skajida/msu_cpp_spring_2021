6084d47d051d8ac58db391861f798c797e48da6a
thiranger <thiranger@yandex.ru>
2021-05-11 23:38:59 +0300

    hw6 fix

--- 06/format.h
+++ 06/format.h
@@ -4,6 +4,12 @@
 #include <sstream>
 #include <string>
 #include <array>
+#include <bitset>
+
+class format_error : public std::logic_error {
+public:
+    explicit format_error(std::string message) : logic_error(message) {}
+};
 
 template <typename T>
 std::string str(T &&ref) {
@@ -15,11 +21,12 @@ std::string str(T &&ref) {
 template <typename... ArgsT>
 std::string format(std::string_view formatString, ArgsT&&... args) {
     std::array<std::string, sizeof...(args)> formattedArgs = { str(std::forward<ArgsT>(args))... };
+    std::bitset<sizeof...(args)> usedArgs;
     std::ostringstream res;
     for (size_t openBrace, closeBrace; (openBrace = formatString.find('{')) != std::string::npos; formatString.remove_prefix(closeBrace + 1)) {
         closeBrace = formatString.find('}', openBrace + 1);
         if (closeBrace == std::string::npos) {
-            throw std::runtime_error("couldn't find closing brace");
+            throw format_error("couldn't find closing brace");
         }
         res << formatString.substr(0, openBrace);
         std::string inputSubString(formatString.substr(openBrace + 1, closeBrace - openBrace - 1));
@@ -27,9 +34,16 @@ std::string format(std::string_view formatString, ArgsT&&... args) {
         size_t arg;
         if (is >> std::noskipws >> arg && is.rdstate() == std::istream::eofbit && arg < formattedArgs.size()) {
             res << formattedArgs[arg];
+            usedArgs[arg] = true;
         } else {
-            throw std::runtime_error("key error: '" + inputSubString + "'");
+            throw format_error("key error: '" + inputSubString + "'");
         }
+    }
+    if (formatString.find('}') != std::string_view::npos) {
+        throw format_error("found unexpected closing brace");
+    }
+    if (sizeof...(args) && !usedArgs[sizeof...(args) - 1]) {
+        throw format_error("got unexpected keys");
     }
     res << formatString;
     return res.str();
--- 06/main.cpp
+++ 06/main.cpp
@@ -10,26 +10,42 @@ enum class EResult {
 };
 
 template <typename... ArgsT>
-EResult test(std::string_view ans, std::string_view formatString, ArgsT&&... args) {
+EResult test(std::string_view ans, std::string_view what, std::string_view formatString, ArgsT&&... args) {
     try {
         if (ans == format(formatString, std::forward<ArgsT&&>(args)...)) {
             return EResult::OK;
         }
         return EResult::WA;
-    } catch (const std::runtime_error &e) {
+    } catch (const format_error &e) {
+        assert(e.what() == what);
         return EResult::RE;
     }
 }
 
 void TestFormat() {
-    assert(test("https://github.com/skajiDa/msu_cpp_spring_2021", "https://github.com/{0}/msu_cpp_spring_2021", "skajiDa") == EResult::OK);
-    assert(test("https://regex101.com/", "https://regex{0}.com/", 101) == EResult::OK);
-    assert(test("one+one = 2", "{1}+{1} = {0}", 2, "one") == EResult::OK);
-    assert(test("wrong answer", "{0} {1}", "wrong", "answeR") == EResult::WA);
-    assert(test("wrong answer", "{0}{1}", "wrong", "answer") == EResult::WA);
-    assert(test("lol_ooo@list.ru", "{0@list.ru", "lol_ooo") == EResult::RE);
-    assert(test("thiranger", "thiran{}", "ger") == EResult::RE);
-    assert(test("lol_ooo@list.ru", "{1}@{2}", "lol_ooo", "list.ru") == EResult::RE);
+    assert(test("https://github.com/skajiDa/msu_cpp_spring_2021", "", "https://github.com/{0}/msu_cpp_spring_2021", "skajiDa") == EResult::OK);
+    assert(test("https://regex101.com/", "", "https://regex{0}.com/", 101) == EResult::OK);
+    assert(test("one+one = 2", "", "{1}+{1} = {0}", 2, "one") == EResult::OK);
+    assert(test("thiranger@mail.ru", "", "{1}@{2}", 938, "thiranger", "mail.ru") == EResult::OK);
+    assert(test("", "", "") == EResult::OK);
+    assert(test("no format", "", "no format") == EResult::OK);
+    assert(test("0", "", "{0}", false) == EResult::OK);
+    assert(test("750-938", "", "{0}{1}{2}{3}", 750, '-', 9, 38) == EResult::OK);
+    assert(test("cheap 'n dale", "", "{10} 'n {11}", 0, true, false, 1, -1, 100, 6, 7, 8, 9, "cheap", "dale") == EResult::OK);
+
+    assert(test("wrong answer", "", "{0} {1}", "wrong", "answeR") == EResult::WA);
+    assert(test("wrong answer", "", "{0}{1}", "wrong", "answer") == EResult::WA);
+
+    assert(test("lol_ooo@list.ru", "couldn't find closing brace", "{0@list.ru", "lol_ooo") == EResult::RE);
+    assert(test("0}@list.ru", "found unexpected closing brace", "0}@list.ru") == EResult::RE);
+    assert(test("0@list}.ru", "found unexpected closing brace", "0@list}.ru") == EResult::RE);
+    assert(test("thiranger", "key error: ''", "thiran{}", "ger") == EResult::RE);
+    assert(test("lol_ooo@list.ru", "key error: '2'", "{1}@{2}", "lol_ooo", "list.ru") == EResult::RE);
+    assert(test("thiranger@mail.ru", "got unexpected keys", "{0}@{1}", "thiranger", "mail.ru", 938) == EResult::RE);
+    assert(test("thiranger", "key error: 'string'", "thiran{string}", "ger") == EResult::RE);
+    assert(test("thiranger", "key error: ' 0'", "thiran{ 0}", "ger") == EResult::RE);
+    assert(test("thiranger", "key error: '0 '", "thiran{0 }", "ger") == EResult::RE);
+    assert(test("thiranger", "key error: '348975342384721239872431092934902'", "thiran{348975342384721239872431092934902}", "ger") == EResult::RE);
 
     std::cerr << "TestFormat is OK" << std::endl;
 }
